a0eb10594c4827e71435721082e0157cae79db08
thepassle <pascalschilp@gmail.com>
2023-09-02 09:13:06 +0200

    fix: strategies

--- strategies.js
+++ strategies.js
@@ -1,15 +1,17 @@
+import { html } from './html.js';
+
 export function NetworkFirst({file, children}) {
-  return fetch(file).catch(() => caches.match(file).then(r => r || children));
+  return html`${fetch(file).catch(() => caches.match(file).then(r => r || children))}`;
 }
 
 export function CacheFirst({file, children}) {
-  return caches.match(file).then(r => r || fetch(file).catch(() => children)); 
+  return html`${caches.match(file).then(r => r || fetch(file).catch(() => children))}`;
 }
 
 export function CacheOnly({file, children}) {
-  return caches.match(file).then(r => r || children);
+  return html`${caches.match(file).then(r => r || children)}`;
 }
 
 export function NetworkOnly({file, children}) {
-  return fetch(file).catch(() => children);
+  return html`${fetch(file).catch(() => children)}`;
 }
