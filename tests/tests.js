d6a2cfe329fcdfffd7b38fc244d2cf11f2d65eeb
thepassle <pascalschilp@gmail.com>
2024-01-14 15:35:01 +0100

    feat: override response options

--- tests/tests.js
+++ tests/tests.js
@@ -4,6 +4,7 @@ import { html } from '../html.js';
 import { renderToString } from '../render.js';
 import { Await, when } from '../await.js';
 import { COMPONENT_SYMBOL } from '../symbol.js';
+import { Router } from '../router.js';
 
 function Foo() {}
 function Bar() {
@@ -412,4 +413,49 @@ describe('renderToString', () => {
     const result = await renderToString(html`<${Html}><h1>welcome ${1}</h1><${Foo} bar=${1} baz="2"/><footer>copyright</footer><//>`)
     assert.equal(result, '<html><body><h1>welcome 1</h1><h2>foo 1 2</h2><footer>copyright</footer></body></html>');
   }); 
+});
+
+globalThis.URLPattern = class URLPattern {
+  exec() {
+    return true;
+  }
+}
+describe('Router', () => {
+  it('creates a default response', async () => {
+    const router = new Router({
+      routes: [
+      {
+        path: '/foo',
+        render: () => html`foo`
+      }
+    ]});
+
+    const response = await router.handleRequest({url: 'https://example.com/foo'});
+    const result = await response.text();
+    assert.equal(result, 'foo');
+    
+    assert.equal(response.headers.get('Content-Type'), 'text/html');
+    assert.equal(response.headers.get('Transfer-Encoding'), 'chunked');
+  });
+
+  it('can override defaults via `options`', async () => {
+    const router = new Router({
+      routes: [
+      {
+        path: '/foo',
+        render: () => html`foo`,
+        options: {
+          status: 300,
+          headers: {
+            'Content-Type': 'foo'
+          }
+        }
+      }
+    ]});
+
+    const response = await router.handleRequest({url: 'https://example.com/foo'});
+    
+    assert.equal(response.status, 300);
+    assert.equal(response.headers.get('Content-Type'), 'foo');
+  });
 });
